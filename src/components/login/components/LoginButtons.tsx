e2207c4dee6892b7d52e8c3b3c40ac43371c7b34
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-04 17:13:28 +0530

    Add reset and login buttons.

--- src/components/login/components/LoginButtons.tsx
+++ src/components/login/components/LoginButtons.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@mui/material";
+
 function LoginButtons(): JSX.Element {
     return (
         <>
+            <Button variant="contained">Reset</Button>
+            <Button variant="contained">Login</Button>
         </>
     );
 }
