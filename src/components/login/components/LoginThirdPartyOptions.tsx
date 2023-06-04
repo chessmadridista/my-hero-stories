2edcc8b7dc97bed805b922afdede52f725e57d47
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-04 17:23:00 +0530

    Modify color of third party login items.

--- src/components/login/components/LoginThirdPartyOptions.tsx
+++ src/components/login/components/LoginThirdPartyOptions.tsx
@@ -1,6 +1,20 @@
+import { AccountCircle, Apple, Facebook, Google } from "@mui/icons-material";
+import { Grid } from "@mui/material";
+
 function LoginThirdPartyOptions(): JSX.Element {
     return (
         <>
+            <Grid container>
+                <Grid item xs={4}>
+                   <Google sx={{ color: "blue" }} />
+                </Grid>
+                <Grid item xs={4}>
+                   <Facebook sx={{ color: "blue" }} />
+                </Grid>
+                <Grid item xs={4}>
+                   <Apple sx={{ color: "blue" }} />
+                </Grid>
+            </Grid>
         </>
     );
 }
