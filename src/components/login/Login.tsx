1cea3070e7983972209bf33af449c95d6d9a46cc
Lavanya Mishra <lavanyamishra1996@gmail.com>
2023-06-04 17:11:35 +0530

    Add input fields.

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,12 @@
 import LoginInputs from "./components/LoginInputs";
 import LoginButtons from "./components/LoginButtons";
 import LoginThirdPartyOptions from "./components/LoginThirdPartyOptions";
-import { Card } from "@mui/material";
+import { Card, CardHeader } from "@mui/material";
 
 function Login(): JSX.Element {
     return (
         <Card sx={{ padding: 2 }}>
+            <CardHeader title="Login" sx={{ color: "blue" }} />
             <LoginInputs />
             <LoginButtons />
             <LoginThirdPartyOptions />
--- src/components/login/components/LoginInputs.tsx
+++ src/components/login/components/LoginInputs.tsx
@@ -1,6 +1,16 @@
+import { Grid, TextField } from "@mui/material";
+
 function LoginInputs(): JSX.Element {
     return (
         <>
+            <Grid container>
+                <Grid item xs={12}>
+                    <TextField id="username" label="Username" type="text" variant="outlined" />        
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField id="password" label="Password" type="password" variant="outlined" />        
+                </Grid>
+            </Grid>
         </>
     );
 }
